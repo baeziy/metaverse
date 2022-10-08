15f2e1c7fc72105a9fa6147f87378480cebc051e
Mustafa Kamal <mustafakamal@Mustafas-Macbook-Pro.local>
2022-10-08 22:11:30 +0500

    added on github

---
+++ jsAssignments/milesToKm/script.js
@@ -0,0 +1,5 @@
+let conversionFactor = 1.60934;
+let miles = prompt("Please enter the distance in miles");
+console.log("Given miles: ", miles);
+let km = miles * conversionFactor;
+console.log("Miles to KM: ", km);
